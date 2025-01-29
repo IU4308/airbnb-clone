b7381f5565ac8473ee4fd17468b75c6d2c0d43c5
Ilya <ilya671qw@gmail.com>
2025-01-29 21:17:51 +0300

    fixed problems

--- app/ui/Search.tsx
+++ app/ui/Search.tsx
@@ -1,22 +1,22 @@
 'use client';
 
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+// import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+// import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/16/solid'
 
 function Search() {
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { replace } = useRouter();
-    const handleSearch = useDebouncedCallback((term: string) => {
-        const params = new URLSearchParams(searchParams);
-        if (term) {
-            params.set('query', term);
-        } else {
-            params.delete('query');
-        }
-        replace(`${pathname}?${params.toString()}`)
-    }, 500)
+    // const searchParams = useSearchParams();
+    // const pathname = usePathname();
+    // const { replace } = useRouter();
+    // const handleSearch = useDebouncedCallback((term: string) => {
+    //     const params = new URLSearchParams(searchParams);
+    //     if (term) {
+    //         params.set('query', term);
+    //     } else {
+    //         params.delete('query');
+    //     }
+    //     replace(`${pathname}?${params.toString()}`)
+    // }, 500)
     return (
         <div className='w-[60%] max-w-5xl relative  max-lg:w-full'>
             {/* max-lg:absolute max-lg:top-[80] max-lg:w-full */}
@@ -24,10 +24,10 @@ function Search() {
                 className="w-[100%] p-4 border border-gray-300 rounded-full drop-shadow shadow-black"
                 type="text"
                 placeholder='Search for an apartment'
-                onChange={(e) => {
-                    handleSearch(e.target.value)
-                }}
-                defaultValue={searchParams.get('query')?.toString()}
+            // onChange={(e) => {
+            //     handleSearch(e.target.value)
+            // }}
+            // defaultValue={searchParams.get('query')?.toString()}
             />
 
             <MagnifyingGlassCircleIcon className='absolute z-10 w-12 right-2 top-1 text-green' />
