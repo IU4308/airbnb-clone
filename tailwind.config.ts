23da5006380647e4b46f7dd0d17611aa99aca659
Ilya <ilya671qw@gmail.com>
2025-01-29 14:13:50 +0300

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
